7c929322e5b9096323fb6aeab6f097f50a051b39
OraniumTech1409 <oraniumtechvelachery@gmail.com>
2022-08-11 20:19:17 +0530

    Reg page added

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class RegistrationPage {
+
+	public RegistrationPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private void regMethod() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
